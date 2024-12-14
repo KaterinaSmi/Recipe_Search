94f56ca9cf282001e75415ce524022739424d3ac
Ekaterina Smirnova <ekaterina.mavritckaia@gmail.com>
2024-12-14 11:49:47 +0200

    Improved index.js

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection setup
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected successfully' + process.env.MONGODB_URI);
   })
